e58182bb751036515bd597549e79c81b2eceb831
Vlad-Leskov <tyabaka@gmail.com>
2024-03-24 20:30:15 +0200

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+const apiKey = '43042751-379763e2b45d181b55d7040e4';
+const baseUrl = 'https://pixabay.com/api/';
+
+function fetchImages(searchQuery) {
+  const url = `${baseUrl}?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Unable to fetch images');
+      }
+      return response.json();
+    })
+    .then(data => data.hits)
+    .catch(error => {
+      console.error(error);
+      return [];
+    });
+}
+
+export { fetchImages };
